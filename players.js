53157322ec86169de7971c835236dbf996aab9ca
michaellidonni <526mlidonni@frhsd.com>
2025-05-13 09:27:09 -0400

    first commit

---
+++ players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+        firstName: "Francisco",
+        lastName: "Lindor",
+        position: "55",
+        age: 31,
+        photo: "https://picsum.photos/200"
+    },
+    {
+        firstName: "Francisco",
+        lastName: "Lindor",
+        position: "55",
+        age: 31,
+        photo: "https://picsum.photos/200"
+    },
+    {
+        firstName: "Francisco",
+        lastName: "Lindor",
+        position: "55",
+        age: 31,
+        photo: "https://picsum.photos/200"
+    },
+    {
+        firstName: "Francisco",
+        lastName: "Lindor",
+        position: "55",
+        age: 31,
+        photo: "https://picsum.photos/200"
+    },
+    {
+        firstName: "Francisco",
+        lastName: "Lindor",
+        position: "55",
+        age: 31,
+        photo: "https://picsum.photos/200"
+    }
+]
