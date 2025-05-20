20850f86c6258066c22b643afca2dab998ad744c
michaellidonni <526mlidonni@frhsd.com>
2025-05-20 11:18:12 -0400

    Added all fighters and put a placeholder photo

--- roster.js
+++ roster.js
@@ -10,8 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     <img src="${p.photo}" class="card-img-top" alt="${p.firstName} ${p.lastName}">
                     <div class="card-body text-center">
                     <h5 class="card-title mb-1">${p.firstName} ${p.lastName}</h5>
-                    <div class="badge badge-position badge-pos-${p.position}'>${p.position}</div>
-                    <p class="small text-muted mb-0" Age${p.age}</p>
+                    <div class="badge badge-position badge-pos-${p.weightClass}">${p.weightClass}</div>
+                    <p class="small text-muted mb-0"> Age ${p.age}</p>
                 </div>
             </div>
             `
